ef292ccf55891bac516cfb2755713cf9ec31e77a
Rohan Bhansali <rohanmbhansali@gmail.com>
2020-04-19 13:00:12 -0400

    Update SimpNet-7_pretrained.py

--- SimpNet-7_pretrained.py
+++ SimpNet-7_pretrained.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  8 18:16:40 2020
-
-@author: rohan
-"""
-
 from numpy import loadtxt
 from keras.models import load_model
  
